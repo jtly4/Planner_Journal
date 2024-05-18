bdab2baf30831964a868865014aec831a7f483e3
jtly4 <166187374+jtly4@users.noreply.github.com>
2024-05-17 17:58:13 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,12 +8,35 @@ const FeatureCard = (props) => {
   )
 }
 
+
+
+
+
 const Features = () => {
+  const featureStyle = {
+    height: "auto",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "33% 33% 33%",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    fontSize: "1.2rem",
+    top: "0"
+  };
+
+  const cardStyle = {
+    width: "fill"
+  };
+
   return (
     <>
-      <FeatureCard title="Create Events" imgTitle="/images/calendar_icon.png"/>
-      <FeatureCard title="Add Memories" imgTitle="/images/journal_icon.png"/>
-      <FeatureCard title="Add Expenses" imgTitle="/images/dollar_icon.png"/>
+    <div /*className='feature_container'*/ style={featureStyle}>
+      <div /*className='feature_card'*/ style={cardStyle}> 
+        <FeatureCard title="Create Events" imgTitle="/images/calendar_icon.png"/>
+        <FeatureCard title="Add Memories" imgTitle="/images/journal_icon.png"/>
+        <FeatureCard title="Add Expenses" imgTitle="/images/dollar_icon.png"/>
+      </div>
+    </div>
     </>
   )
 };
