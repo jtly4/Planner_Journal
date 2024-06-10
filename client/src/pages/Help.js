57c2c76e10f157c2356285c433f0428de9ebfd30
rachelmgk <99305864+rachelmgk@users.noreply.github.com>
2024-06-10 15:06:24 -0700

    Add files via upload

---
+++ client/src/pages/Help.js
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+  
+function MyForm() {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // alert(inputs);
+        alert("Submitted!")
+    }
+  
+    return (
+        <form onSubmit={handleSubmit} action="/help_form" method="POST">
+        <label className='help_label'>Email Adress:</label><br />
+        <input 
+            className='help_input'
+            type="email" 
+            name="email" 
+            placeholder = "Email"
+            required
+            value={inputs.email || ""} 
+            onChange={handleChange}
+        /><br/>
+        <label className='help_label'> Subject:</label><br />
+        <input 
+            className='help_input'
+            type="text" 
+            name="subject" 
+            placeholder = "Subject"
+            minLength={1}
+            required
+            value={inputs.subject || ""} 
+            onChange={handleChange}
+        /><br />
+        <label className='help_label'>Message:</label><br />
+        <textarea 
+            className='help_input'
+            id = "help_message"
+            type="text" 
+            name="message" 
+            placeholder = "Your concern here..."
+            minLength={1}
+            required
+            value={inputs.message || ""} 
+            onChange={handleChange}
+        /><br />
+            <input type="submit" id="help-submit-btn"/>
+        </form>
+    )
+}
+
+function HelpBox() {
+    return (
+        <>
+            <p className='helpBox_txt'>Have a question or concern? Check our <Link to="/FAQ">FAQs</Link> </p>
+            <p className='helpBox_txt'>Have a question that you do not see answered? Fill out this form with your concern and we will get back to you as soon as we can!</p>
+            <p className='helpBox_txt'>Response times may vary between 3 - 12 business days.</p>
+        </>
+    )
+}
+function Help() {
+    return(
+        <>
+            <h2 className="page-title">Help / Contact</h2>
+            <MyForm/>
+        </>
+    )
+};
+
+export default Help;
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,118 @@
+import { useState, useEffect } from 'react';
+import axios from "axios"
+
+  
+function MyForm() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordConfirm, setPasswordConfirm] = useState('')
+    const [error, setErrror] = useState(null)
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        console.log(email, password, passwordConfirm)
+        const newUser = {
+            email, password, passwordConfirm
+        }
+        const response = await fetch('http://localhost:5000/api/users', {
+            method: 'POST',
+            body: JSON.stringify(newUser),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await response.json()
+        if(!response.ok) {
+            setErrror(json.error)
+        }
+        if(response.ook) {
+            setEmail('')
+            setPassword('')
+            setPasswordConfirm('')
+            setErrror(null)
+            console.log('New user added', json)
+        }
+        // axios.post("/api/users", newUser)
+        // axios.post("http://localhost:5000/users", newUser)
+    }
+  
+    return (
+        <form onSubmit={handleSubmit} className="sign-up-form">
+            {/* action="/account_creation" method="POST" */}
+        {/* // <form> */}
+            <input 
+                className='signUp_input'
+                type="email" 
+                name="email" 
+                placeholder = "Email"
+                required
+                value={email} 
+                // onChange={handleChange}
+                onChange = {(e) => setEmail(e.target.value)}
+            /><br/>
+            <input 
+                className='signUp_input'
+                type="password" 
+                name="password" 
+                placeholder = "Password"
+                minLength={8}
+                required
+                value={password} 
+                // onChange={handleChange}
+                onChange = {(e) => setPassword(e.target.value)}
+            /><br />
+            <input 
+                className='signUp_input'
+                type="password" 
+                name="passwordConfirm" 
+                placeholder = "Confirm Password"
+                minLength={8}
+                required
+                value={passwordConfirm} 
+                // onChange={handleChange}
+                onChange = {(e) => setPasswordConfirm(e.target.value)}
+            /><br />
+            <input type="submit" id="signup-btn" onClick={handleSubmit}/>
+            {error && <div className='error'>{error}</div>}
+            {/* <button type="button" id="signup-btn" className = "btn btn-lg btn-info">Sign Up</button> */}
+        </form>
+    )
+  }
+  
+//   const SignUp = () => {
+function SignUp() {
+    const [users, setUsers] = useState([{
+      email:'',
+      password:''
+    }])
+
+    useEffect(() => {
+      const fetchUsers = async () => {
+        const response = await fetch('http://localhost:5000/api/users')
+        const json = await response.json()
+        if(response.ok) {
+          setUsers(json)
+        }
+      }
+      fetchUsers()
+    }, [])
+
+  return(
+    <>
+      {/* <div className="users">
+        {users && users.map((user) =>
+          <p key = {user.password}>{user.email}</p>
+        )}
+      </div> */}
+            <h2 className="page-title">Sign Up</h2>
+            <div className='signUp_box'>
+                <MyForm/>
+            </div>
+        </>
+    )
+};
+
+export default SignUp;
